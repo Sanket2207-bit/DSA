26afb81fdec1eefaf78141876b0ff8e4438a4746
Sanket2207-bit <sanketofficial24@gmail.com>
2026-07-29 23:29:36 +0530

    Learn Maths function

---
+++ Maths fn & conditions/script.js
@@ -0,0 +1,45 @@
+// Maths functions 
+
+// Ceil function decimal value ko next value bna deta h 1.23 = 2 , 2.1 = 2
+console.log(Math.ceil(1.23));
+
+// Floor function is opposite of ceil function 1.23 = 1 , 2.9 = 2
+console.log(Math.floor(1.23));
+
+// Round function roundoff the no if no greater than or equal to 5 it will round of to next no if smaller than 5 it will round of it the before no
+console.log(Math.round(8.5));
+console.log(Math.round(8.6));
+console.log(Math.round(8.4));
+
+// Abs function convert negative value into positive value
+console.log(Math.abs(-98));
+
+// Trunc function remove the decimal part and return integer part
+console.log(Math.trunc(78.9472349864387));
+
+// Pow function convert the no to its given power
+console.log(Math.pow(3 , 5));
+
+// Max function return the maximum value from a group of values
+console.log(Math.max(10 , 989 , 1001 , 22));
+
+// Min function return the mainimum value from a group of values
+console.log(Math.min(10 , 989 , 1001 , 22));
+
+// Sqrt function give the square root of the no
+console.log(Math.sqrt(144));
+
+// Cbrt function give the cube root of the no
+console.log(Math.cbrt(512));
+
+// Random function give random value between 0 and 1
+console.log(Math.random());
+
+let a = 973.9367872
+console.log(a.toFixed(3)); // tofixed function will return exaclty 3 values after decimal , tofixed return no as string 
+
+
+// Question Practice
+
+
+ 
