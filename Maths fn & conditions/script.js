2d1a258fd8d670faffccc4f531eb0b0d3a28dd80
Sanket2207-bit <sanketofficial24@gmail.com>
2026-07-30 21:25:58 +0530

    Questions practice invovling maths functions

--- Maths fn & conditions/script.js
+++ Maths fn & conditions/script.js
@@ -35,11 +35,50 @@ console.log(Math.cbrt(512));
 // Random function give random value between 0 and 1
 console.log(Math.random());
 
-let a = 973.9367872
-console.log(a.toFixed(3)); // tofixed function will return exaclty 3 values after decimal , tofixed return no as string 
-
+let ab = 973.9367872
+console.log(ab.toFixed(3)); // tofixed function will return exaclty 3 values after decimal , tofixed return no as string 
 
 // Question Practice
+// Q1 Find compount interest of the vales given by user
+CP = A - P (A = p*(1+r/100)^t)
+let p = prompt("Enter principle amount")
+let r = prompt("Enter rate of interest")
+let t = prompt("Enter duration of loan")
+console.log((p * Math.pow(1+r/100 , t)) - p);
 
+// Q2 Generate Otp
+let otp = Math.random() * 9000 + 1000
+let fdig = Math.trunc(otp)
+console.log(fdig);
 
- 
+// Q3 Area of Triangle by heron's formula
+// Heron's formula for triangle is sqrt(s(s-a)(s-b)(s-c)) and s = (a+b+c)/2
+let a = Number(prompt("Enter side1 of a traingle"))
+let b = Number(prompt("Enter side2 of a traingle"))
+let c = Number(prompt("Enter side3 of a traingle"))
+let s = (a + b + c)/2
+console.log(Math.sqrt(s*(s - a)*(s - b)*(s - c)))
+
+// Q4 Circumference of Circle
+let rad = Number(prompt("Enter radius of a circle"))
+console.log(2 * 3.14 * rad)
+
+// IF-Else problems
+// Q1 Accept 2 number and print the greatest number between them.
+let num1 = Number(prompt("Enter num1"))
+let num2 = Number(prompt("Enter num2"))
+if(num1>num2){
+    console.log(`num1(${num1}) is greater than num2(${num2})`)
+}
+else{
+    console.log(`num2(${num2}) is greater than num1(${num1})`)
+}
+
+// Q2 Accept an integer and check whether is it odd or even.
+let int1 = Number(prompt("Enter your integer"))
+if(int1%2!=0){
+    console.log(`${int1} is odd`)
+}
+else(
+    console.log(`${int1} is even`)
+)
